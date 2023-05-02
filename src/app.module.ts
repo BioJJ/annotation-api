507a0f25222be5d147cedd444a7d6288d13a3d16
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2023-05-02 01:14:21 -0400

    refactor: ♻️ config modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { PersonModule } from './person/person.module'
+import { AddressModule } from './address/address.module'
+import { HttpModule } from '@nestjs/axios'
+import { AnnotationModule } from './annotation/annotation.module'
+import { ConfigModule } from '@nestjs/config'
+import * as Joi from '@hapi/joi'
+import { DatabaseModule } from './database/database.module'
+import { AuthModule } from './auth/auth.module'
 
 @Module({
-	imports: [],
+	imports: [
+		PersonModule,
+		AddressModule,
+		AnnotationModule,
+		HttpModule,
+		ConfigModule.forRoot({
+			validationSchema: Joi.object({
+				DATABASE_URL: Joi.string().required(),
+				PORT: Joi.number()
+			})
+		}),
+		DatabaseModule,
+		AuthModule
+	],
 	controllers: [AppController],
 	providers: [AppService]
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { ValidationPipe } from '@nestjs/common'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
-	await app.listen(3000)
+
+	const config = new DocumentBuilder()
+		.setTitle('ANNOTATION-API')
+		.setDescription('ANNOTATION-API')
+		.setVersion('1.0')
+		.build()
+
+	const document = SwaggerModule.createDocument(app, config)
+	SwaggerModule.setup('api', app, document)
+
+	app.useGlobalPipes(
+		new ValidationPipe({
+			transform: true,
+			whitelist: true,
+			forbidNonWhitelisted: true
+		})
+	)
+	app.enableCors()
+
+	await app.listen(process.env.PORT ? parseInt(process.env.PORT) : 3000)
 }
 bootstrap()
